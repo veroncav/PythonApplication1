036a8e526d39acb5a602a5f8b7ee26c5f5f5de06
Veronika Vanitseva <Veronika.Vanitseva@tthkee.onmicrosoft.com>
2024-11-20 16:36:31 +0200

    Ülesanne 1 oled tehtud

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,35 @@
-print ("Hello World")
+from random import* # #*-kõik funksioonid, randint as rd сокращение
+from math import* #pi
+# #Ülesanne 1
+# print ("Hello World") #lower()-aaa,upper()-AAA,capitalize()-Aaa
+# name=input("Mis on sinu nimi? ").capitalize()
+# print("Hello World!Tervitan sind", name)
+# print("Hello World!Tervitan sind"+ name)
+# age=int(input("Kui vana sa oled?" ))
+# print("Hello World! Tervitan sind "+name+"Sa oled ",age,"aastat vana")
+# print(f"Hello World! Tervitan sind {name} Sa oled {age} aastat vana")
+
+
+#Ülesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(type(vanus)),print(type(eesnimi)),print(type(pikkus)),print(type(kas_käib_koolis))
+
+
+#Ülesanne 3
+# kokku=randint(1,1000)
+# print(f"Kokku on {kokku} kommi")
+# kommi=int(input("Mittu kommi sa tahad?"))
+# kokku=kokku-kommi
+# print(f"Jääk on {kokku} kommi")
+
+
+#Ülesanne 4
+print("Läbimõõdu leidmine ")
+#l
+#-ümbermõõt
+l=float(input("ümbermõõt: "))
+d=l/pi
+print(f"Läbimõõdu suurus on {d}")
